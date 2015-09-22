9271da4151a94b08bc94de4804cbc9047dcd4211
sebastianjonas <sebastianjonas@ymail.com>
2015-09-22 14:24:02 +0200

    vor Kapitel 4

---
+++ client/templates/posts/post_item.js
@@ -0,0 +1,10 @@
+/**
+ * Created by sebastiankopf on 22.09.15.
+ */
+Template.postItem.helpers({
+   domain: function () {
+       var a = document.createElement('a');
+       a.href = this.url;
+       return a.hostname;
+   }
+});
