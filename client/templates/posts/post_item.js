7fbdf6bb7447fac8e14715a2f984edc5f616e7cb
sebastianjonas <sebastianjonas@ymail.com>
2015-09-22 17:09:48 +0200

    kapitel 5.3 commit

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,10 +1,7 @@
-/**
- * Created by sebastiankopf on 22.09.15.
- */
 Template.postItem.helpers({
-   domain: function () {
-       var a = document.createElement('a');
-       a.href = this.url;
-       return a.hostname;
-   }
+    domain: function() {
+        var a = document.createElement('a');
+        a.href = this.url;
+        return a.hostname;
+    }
 });
