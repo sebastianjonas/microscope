56a912746f97347d205ad1a1fc02c456a3b16cc6
Sebastian Kopf <sebastian.kopf@gmail.com>
2015-10-27 15:47:57 +0100

    Kapitel 10 Comments fertig

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -6,8 +6,17 @@ Template.postItem.helpers({
     },
     ownPost: function(){
       return this.userId === Meteor.userId();
-    },
+    }
+
+    //CommentsCount wurde zuerst über CommentCollection ermittelt
+    //jetzt wird es bei jedem CommentInsert direkt auf der Posts Collection
+    //erweitert.
+    //Damit mapped Meteor nicht mehr auf den Helper, sondern auf das Attribut
+    // der aktuelle im Datakontext verfügbaren Collection (nach welcher Prio?).
+    /*
     commentsCount: function(){
         return Comments.find({postId: this._id}).count();
     }
+    */
+
 });
