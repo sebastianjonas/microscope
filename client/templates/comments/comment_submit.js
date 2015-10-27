cca42b5d1c06e317cb6ea99412d1b30a9d15339d
sebastianjonas <sebastianjonas@ymail.com>
2015-10-27 14:30:45 +0100

    Comment_submit file getauscht und rennt

--- client/templates/comments/comment_submit.js
+++ client/templates/comments/comment_submit.js
@@ -1,42 +1,38 @@
-/**
- * Created by sebastiankopf on 27.10.15.
- */
-Template.commentSubmit.onCreated(function () {
-    Session.set('commentSubmitErrors',{});
+Template.commentSubmit.onCreated(function() {
+    Session.set('commentSubmitErrors', {});
 });
 
 Template.commentSubmit.helpers({
-    errorMessage: function(field){
+    errorMessage: function(field) {
         return Session.get('commentSubmitErrors')[field];
     },
     errorClass: function (field) {
-        return Session.get('commentSubmitErrors')[field]?'has-error' : '';
+        return !!Session.get('commentSubmitErrors')[field] ? 'has-error' : '';
     }
 });
 
 Template.commentSubmit.events({
-   'submit form': function (event, template){
-       event.preventDefault();
+    'submit form': function(e, template) {
+        e.preventDefault();
 
-       var $body = $(event.target).find('[name=body]').val();
-       var postId =  template.data_id
+        var $body = $(e.target).find('[name=body]');
+        var comment = {
+            body: $body.val(),
+            postId: template.data._id
+        };
 
-       var comment = {
-           body: $body,
-           postId: postId
-       };
+        var errors = {};
+        if (! comment.body) {
+            errors.body = "Please write some content";
+            return Session.set('commentSubmitErrors', errors);
+        }
 
-       var errors = {};
-       if (!comment.body){
-           errors.body = "Please write some content";
-           return Session.set('commentSubmitErrors', errors);
-       }
-       Meteor.call('commentInsert', comment, function (error, commentId) {
-           if (error){
-               throwError(error.reason);
-           } else {
-               $body.val('');
-           }
-       })
-   }
+        Meteor.call('commentInsert', comment, function(error, commentId) {
+            if (error){
+                throwError(error.reason);
+            } else {
+                $body.val('');
+            }
+        });
+    }
 });
