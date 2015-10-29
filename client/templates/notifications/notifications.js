07b156ac9210473ca55d17e46d76dafd0d6c22c2
Sebastian Kopf <sebastian.kopf@gmail.com>
2015-10-29 11:19:49 +0100

    funkt weiss nciht warum

--- client/templates/notifications/notifications.js
+++ client/templates/notifications/notifications.js
@@ -3,24 +3,23 @@
  */
 Template.notifications.helpers({
     notifications: function() {
-       var notifi1 = Notifications.find({userId: Meteor.userId(), read: false});
-        return notifi
+        var notifi1 = Notifications.find({userId: Meteor.userId(), read: false});
+        return notifi1;
     },
     notificationCount: function(){
         var notifi2 = Notifications.find({userId: Meteor.userId(), read: false}).count();
-        return notifi
+        return notifi2;
     }
 });
 
 Template.notificationItem.helpers({
-   notificationPostPath: function (){
-       var notifi3 =  Router.routes.postPage.path({_id: this._id});
-       return notifi3
-   }
+    notificationPostPath: function() {
+        return Router.routes.postPage.path({_id: this.postId});
+    }
 });
 
 Template.notificationItem.events({
-    'click a': function () {
+    'click a': function() {
         Notifications.update(this._id, {$set: {read: true}});
     }
-})
+});
